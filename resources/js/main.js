2d2023f578ca6b24be86c7454ed93fa3d3096cad
jukebox303 <samuel_eslick@knights.ucf.edu>
2021-11-25 01:27:29 -0500

    Project develpement cycle moved to observable

--- resources/js/main.js
+++
@@ -1,8 +0,0 @@
-import { hex2rgb, computeModelExtent, loadModelFromURL } from './utils.js';
-
-modelObject = await loadModelFromURL('/resources/models/link.obj');
-
-//const modelDim = computeModelExtent(modelObject);
-//console.log(modelDim);
-const hex = '#FFFFFF';
-console.log(hex2rgb(hex));
